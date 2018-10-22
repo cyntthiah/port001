a414cfc8ab45e2994e3b718131fb03a70b307b58
cyntthiah <cyntthiah@gmail.com>
2018-10-21 22:56:00 -0300

    links

---
+++ app3.js
@@ -0,0 +1,26 @@
+window.onload = ()=>{
+    document.getElementById('repoSocial').addEventListener('click', ()=>{
+        window.location.href = "https://github.com/cyntthiah/socialnetworkfemmes";
+       
+    });
+    document.getElementById('demoSocial').addEventListener('click', ()=>{
+        window.location.href = "https://socialnetwork2-36f58.firebaseapp.com";
+    })
+}
+    document.getElementById('repoNpm').addEventListener('click' ,()=>{
+        window.location.href = "https://github.com/cyntthiah/scl-2018-01-FE-markdown";
+    });
+
+    document.getElementById('demoNpm').addEventListener('click' ,()=>{
+        window.location.href = "https://www.npmjs.com/package/cynthia-md-links";
+
+    });
+
+
+    document.getElementById('repoData').addEventListener('click' ,()=>{
+    window.location.href = "https://github.com/cyntthiah/scl-2018-05-bc-core-pm-datadashboard";
+});
+
+    document.getElementById('demoData').addEventListener('click' ,()=>{
+    window.location.href = "https://valerianotvalentina.github.io/scl-2018-05-bc-core-pm-datadashboard/src/";
+    });
